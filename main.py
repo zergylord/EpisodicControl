43f4e6cb3661cf16a70085cbc1f81a6245c2fcdd
Steven Hansen <zergylord@gmail.com>
2016-08-01 08:53:48 -0700

    removes based on usage

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import gym
 import numpy as np
-import pylab
+import matplotlib.pyplot as plt
 import scipy.misc as misc
 import time
 from sklearn.neighbors import NearestNeighbors
-env = gym.make('SpaceInvaders-v0')
+env = gym.make('Frostbite-v0')
 n_act = env.action_space.n
+print(n_act)
 knn = []
 for a in range(n_act):
     knn.append(NearestNeighbors(n_neighbors=11))
@@ -51,6 +52,7 @@ rep_dim = 64
 M = np.random.randn(s_dim,rep_dim)
 S = np.zeros((n_act,mem_size,rep_dim))
 R = np.zeros((n_act,mem_size,))
+last_used = np.tile(np.asarray(range(-mem_size,0)),[n_act,1])
 mem_ind = np.zeros((n_act,),dtype=int)
 episode_states = []
 for a in range(n_act):
@@ -61,15 +63,15 @@ cumr = 0.0
 episodes = 0.0
 warming = True
 refresh = int(1e3)
+plt.ion()
+r_hist = []
 for i in range(int(1e7)):
     obs = process_obs(s)
     
     if not warming:
         action,nearby = get_action(np.matmul(obs,M))
-        #move recently used indices to the back of the list (so they wont be killed)
         for a in range(n_act):
-            S[a] = np.insert(np.delete(S[a],nearby[a],0),mem_ind[a],S[a][nearby[a]],0)
-            R[a] = np.insert(np.delete(R[a],nearby[a],0),mem_ind[a],R[a][nearby[a]],0)
+            last_used[a,nearby[a]] = i
 
     if np.random.rand() < .005 or warming:
         action = env.action_space.sample()
@@ -92,29 +94,31 @@ for i in range(int(1e7)):
         #print(i,'done!',Ret)
         cumr+=Ret
         if warming:
-            if i > 1:
+            if i > 250:
                 warming = False
         s = env.reset()
         for a in range(n_act):
             n_reps = len(episode_states[a])
             if n_reps > 0:
                 episode_reps = np.matmul(np.asarray(episode_states[a]),M)
-                if mem_ind[a] + n_reps >= mem_size:
-                    remaining = mem_size-mem_ind[a]
-                    overflow = n_reps-remaining
-                    S[a,mem_ind[a]:] = episode_reps[:remaining]
-                    R[a,mem_ind[a]:] = Ret
-                    S[a,:overflow] = episode_reps[remaining:]
-                    R[a,:overflow] = Ret
-                    mem_ind[a] = mem_size
-                else:
-                    S[a,mem_ind[a]:mem_ind[a]+n_reps] = episode_reps
-                    R[a,mem_ind[a]:mem_ind[a]+n_reps] = Ret
+                replace_these = np.argpartition(last_used[a],n_reps-1)[:n_reps]
+                last_used[a][replace_these] = i
+                S[a,replace_these] = episode_reps
+                R[a,replace_these] = Ret
+                if mem_ind[a] + n_reps < mem_size:
                     mem_ind[a] += n_reps
+                    #print(mem_ind[a],len(last_used[a][last_used[a] >= 0.0])) 
+                else:
+                    mem_ind[a] = mem_size
                 knn[a].fit(S[a][:mem_ind[a]])
                 episode_states[a] = []
         Ret = 0
     if i >0 and i % refresh == 0:
+        plt.clf()
+        #plt.plot(last_used[0])
+        r_hist.append(cumr/episodes)
+        plt.plot(r_hist)
+        plt.pause(.1)
         print(i,cumr/episodes,time.clock()-cur_time)
         episodes = 0.0
         cumr = 0.0
